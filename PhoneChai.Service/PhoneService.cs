e011e4b91638dbac215fcd1dee012b0dc7b88b0e
linckon <linckon@live.com>
2016-03-16 00:55:05 +0600

    added phoneservice

---
+++ PhoneChai.Service/PhoneService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhoneChai.Model;
+using PhoneChai.Repository;
+using PhoneChai.ViewModel;
+
+namespace PhoneChai.Service
+{
+    public class PhoneService
+    {
+        private BusinessDbContext _dbContext;
+
+        public PhoneService(BusinessDbContext db)
+        {
+            _dbContext = db;
+        }
+
+        public List<PhoneViewModel> GetAll()
+        {
+
+            PhoneRepository repository = new PhoneRepository(_dbContext);
+
+            IQueryable<Phone> queryable = repository.GetAll();
+            var phoneViewModels = queryable.ToList().Select(x => new PhoneViewModel(x)).ToList();
+            return phoneViewModels;
+        }
+    }
+}
