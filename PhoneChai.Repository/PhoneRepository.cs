56f42c391dbbef5d905dc7aab86967bcc272ffbf
linckon <linckon@live.com>
2016-03-16 00:26:25 +0600

    added phonerepository

---
+++ PhoneChai.Repository/PhoneRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhoneChai.Model;
+
+namespace PhoneChai.Repository
+{
+    public abstract class BaseRepository
+    {
+        public BusinessDbContext Db  { get; set; }
+
+        protected BaseRepository(BusinessDbContext db)
+        {
+            Db = db;
+        }
+    }
+    public class PhoneRepository:BaseRepository
+    {
+        public PhoneRepository(BusinessDbContext db):base(db)
+        {
+            
+        }
+        public IQueryable<Phone> GetAll()
+        {
+            return Db.Phones.AsQueryable();
+        }
+    }
+}
