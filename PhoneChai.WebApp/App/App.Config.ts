daf5d638468e1ed41af4e8ff66b9112c7ad6bd68
linckon <linckon@live.com>
2016-03-16 01:26:59 +0600

    added phonechai.webapp

---
+++ PhoneChai.WebApp/App/App.Config.ts
@@ -0,0 +1,32 @@
+﻿module App {
+    export class AppConfig {
+
+        private stateProvider: angular.ui.IStateProvider;
+        private urlProvider: angular.ui.IUrlRouterProvider;
+
+        static $inject = ["$stateProvider", "$urlRouterProvider"];
+        constructor($stateProvider: angular.ui.IStateProvider, $urlRouterProvider: angular.ui.IUrlRouterProvider) {
+            this.stateProvider = $stateProvider;
+            this.urlProvider = $urlRouterProvider;
+            console.log('i m in appconfig.ts');
+            $stateProvider
+                .state("root", {
+                    abstract: true,
+                    url: "",
+                    template: "<div ui-view class=\"container-fluid slide\"></div>",
+                })
+                .state("root.home", {
+                    url: "/",
+                    views: {
+                        "":
+                        {
+                            template: "<h1>Hello.</h1>"
+                        }
+                    }
+                });
+        }
+    }
+
+    angular.module("app", ["ui.router"]);
+    angular.module("app").config(AppConfig);
+}
---
+++ PhoneChai.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhoneChai.WebApp.WebApiApplication" Language="C#" %>
