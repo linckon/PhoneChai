f8afd6cbbf8e6dc93aac8fd3720637ded2c25509
linckon <linckon@live.com>
2016-03-16 04:01:02 +0600

    added PhoneQueryController

---
+++ Source/PhoneChai.WebApp/Controllers/PhoneQueryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PhoneChai.Model;
+using PhoneChai.Service;
+using PhoneChai.ViewModel;
+
+namespace PhoneChai.WebApp.Controllers
+{
+    public class PhoneQueryController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            PhoneService phoneService = new PhoneService(new BusinessDbContext());
+            List<PhoneViewModel> phoneViewModels = phoneService.GetAll();
+            return Ok(phoneViewModels);
+        }
+    }
+}
